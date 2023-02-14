7c5ab6569e5771f5f4f451733ea6e5e005153dc3
KuanHsiaoKuo <kuanhsiaokuo@gmail.com>
2023-02-14 23:35:06 +0800

    restcc_daily_scraper dirty commit

--- scraper_collections/README.md
+++ scraper_collections/README.md
@@ -13,3 +13,16 @@
 ```shell
 cargo run --example <exampels中的示例文件>
 ```
+
+#### rustcc_daily_scraper
+
+> reqwest_tutorial.rs -> 基础版本
+
+```mermaid
+flowchart TD
+1["爬取多少页"]
+2["每一个获取日报标题与链接"]
+3["每个日报获取内容"]
+4["保存为结构体</br>DailyItem"]
+1 --> 2 -->3-->4
+```
--- scraper_collections/examples/rustcc_daily_scraper_version1.rs
+++ scraper_collections/examples/rustcc_daily_scraper_version1.rs
@@ -1,7 +1,7 @@
 use regex::Regex;
 use reqwest::Client;
 use reqwest::header::{HeaderMap, HeaderValue};
-use select::{document::Document, document::Find, predicate::Attr};
+use select::{document::Document, predicate::Attr};
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug, Default)]
@@ -13,7 +13,7 @@ struct DailyItem {
     pub publish_date: String,    // 日报日期
 }
 
-async fn get_page(url: &str, client: &Client, page: &dyn ToString) -> Result<Document, reqwest::Error> {
+async fn get_page(url: &str, client: &Client) -> Result<Document, reqwest::Error> {
     let mut headers = HeaderMap::new();
     headers.insert("Accept", HeaderValue::from_str("text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9").unwrap());
     // headers.insert("Accept-Encoding", HeaderValue::from_str("gzip, deflate, br").unwrap());
@@ -32,13 +32,6 @@ async fn get_page(url: &str, client: &Client, page: &dyn ToString) -> Result<Doc
     headers.insert("sec-ch-ua-mobile", HeaderValue::from_str("?0").unwrap());
     headers.insert("sec-ch-ua-platform", HeaderValue::from_str("\"macOS\"").unwrap());
     // let res = client.get(url).send().await?;
-    // let page_params = [("current_page", page.to_string().as_str())];
-    // let page_params = vec![("current_page", page.to_string().as_str())];
-    let page_params = vec![("current_page".to_string(), page.to_string())];
-    // let query_string = encode(page_params.iter().map(|(k, v)| (k.to_owned(), v.to_string())));
-    let query_string = kv_pair_to_query_string(page_params);
-    println!("{}", query_string);
-    // let page_url = format!("{}?{}", url, reqwest::get_params(page_params));
     let res = client.get(url).headers(headers).send().await?;
     println!("url: {}", &res.url().as_str());
     let body = res.text().await?;
@@ -46,69 +39,51 @@ async fn get_page(url: &str, client: &Client, page: &dyn ToString) -> Result<Doc
     Ok(Document::from(body.as_str()))
 }
 
-// fn kv_pair_to_query_string(params: Vec<(&str, &str)>) -> String {
-fn kv_pair_to_query_string(params: Vec<(String, String)>) -> String {
-    params
-        .iter()
-        .map(|(k, v)| format!("{}={}", k, v))
-        .collect::<Vec<String>>()
-        .join("&")
-}
+// fn get_publish_date<'a>(title: String) -> &'a str{
+//     let date_re = Regex::new(r"(\d{4}-\d{2}-\d{2})").unwrap();
+//     let publish_date = date_re.captures(title.as_str()).unwrap().get(1).unwrap().as_str();
+//     publish_date // returns a value referencing data owned by the current function
+// }
 
-fn get_publish_date(title: String) -> &'static str{
+fn get_publish_date(title: String) -> String {
     let date_re = Regex::new(r"(\d{4}-\d{2}-\d{2})").unwrap();
-    // let publish_date = date_re.captures(title.as_str()).unwrap().get(1).unwrap().as_str();
-    // ----------------------------------------------------^^^^^^ here to match.
-    let publish_date = match date_re.captures(title.as_str()) {
-        Some(captured) => captured.get(1).unwrap().as_str(), // 这里unwrap()之后只有as_str()方法, 没有to_string()
-        // None => format!("Unable to extract date from {}", title).as_str() // temporary value is freed at the end of this statement
-        // None => "Unable to extract date from {title}" // str is equal to &'static str ?
-        None => {
-            let temp = format!("Unable to extract date from {}", title);
-            &temp[..]
-        }
-    };
-    publish_date
+    let publish_date = date_re.captures(title.as_str()).unwrap().get(1).unwrap().as_str();
+    publish_date.to_string()
 }
 
-async fn page_extractor<'a>(document: Document, page: i32) -> Option<Vec<DailyItem>>{
-    let title_class = "title left";
-    let exist_elements = document.find(Attr("class", title_class));
-    let exist_ele_vec = document.find(Attr("class", title_class)).collect::<Vec<_>>();
-    let exist_count = exist_ele_vec.len();
-    if exist_count == 0 {
-        // if exist_elements.clone().count() == 0 {
-        println!("No elements found with `{}` in page {}", title_class, page);
-        None
-    } else {
-        let mut exist_nodes = Vec::new();
-        // println!("found {exist_count} elements in page {page}");
-        // Some(exist_elements)
-        for node in exist_elements {
-            let title = node.text();
-            let url = node.attr("href").unwrap_or("");
-            let publish_date = get_publish_date(title.clone());
-            println!("Title: {}\nLink: {}\n", title, url);
-            let node_item = DailyItem { title, url: url.to_string(), publish_date: publish_date.to_string() };
-            println!("node_item: {}\n", serde_json::to_string(&node_item).unwrap());
-            exist_nodes.push(node_item);
-        }
-        Some(exist_nodes)
-    }
-}
 
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
     let client = Client::new();
     let base_url = "https://rustcc.cn/section?id=f4703117-7e6b-4caf-aa22-a3ad3db6898f";
-    let mut page = 1;
-    while page < 60 {
-        let document = get_page(base_url, &client, &page).await.unwrap();
-        let exist_elements = page_extractor(document, page.clone()).await.unwrap();
-        for node in exist_elements{
-            println!("get_node_item: {}\n", serde_json::to_string(&node).unwrap());
+
+    let document = get_page(base_url, &client).await?;
+    let title_class = "title left";
+    // let exist_elements = document.find(Class(title_class));
+    let exist_elements = document.find(Attr("class", title_class));
+    let exist_ele_vec = document.find(Attr("class", title_class)).collect::<Vec<_>>();
+    let exist_count = exist_ele_vec.len();
+    if exist_count == 0 {
+        // if exist_elements.clone().count() == 0 {
+        println!("No elements found with `{}`", title_class);
+    } else {
+        println!("found");
+        for node in exist_elements {
+            // for node in document.find(Attr("class", title_class)) {
+            let title = node.text();
+            let url = node.attr("href").unwrap_or("");
+            // {
+            //     let publish_date = date_re.captures(title.as_str()).unwrap().get(1).unwrap().as_str();
+            // }
+            // let publish_date = {
+            //     let temp = date_re.captures(title.as_str()).unwrap().get(1).unwrap().as_str();
+            //     temp
+            // };
+            let publish_date = get_publish_date(title.clone());
+            println!("Title: {}\nLink: {}\n", title, url);
+            let node_item = DailyItem { title, url: url.to_string(), publish_date };
+            println!("node_item: {}\n", serde_json::to_string(&node_item).unwrap());
         }
-        page += 1;
     }
 
     Ok(())
